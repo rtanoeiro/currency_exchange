de2fdc209a7b77c2ae0626d046a2c459ed90dc31
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-06-11 23:03:40 +1000

    Updated libraries

--- utils/currency_exchange.py
+++ utils/currency_exchange.py
@@ -1,7 +1,14 @@
 """Module to hold all functions to convert into different Currency Exchanges"""
 
-from forex_python.converter import CurrencyRates
-from .available_currencies import available_currencies
+import datetime
+from io import BytesIO
+from urllib.request import urlopen, urlretrieve
+from zipfile import ZipFile
+
+import pandas as pd
+import wget
+from currency_converter import ECB_URL, CurrencyConverter
+
 
 class CurrencyExchange:
     """
@@ -10,13 +17,20 @@ class CurrencyExchange:
 
     def __init__(self) -> None:
         self.default_currency = "AUD"
-        self.cr = CurrencyExchange()
-    
-    def get_available_currency_rates(self) -> None:
-        
-        for currencie in available_currencies:
-            print(f"{currencie}: {currencie}")
+        self.currency_exchange = CurrencyConverter(currency_file=ECB_URL)
 
+    def get_available_currency_rates(self) -> list:
+        """
+        Function to get all available currencies
+        """
+
+        # TODO: STUDY urlopen, urlretrieve
+        file_path = urlretrieve(url=ECB_URL)
+        data = pd.read_csv(file_path[0], compression='zip')
+        available_currencies = [currency if len(currency)==3 else "" for currency in data.columns][1:-1]
+
+        return available_currencies
+    
     def convert_into(self, currency: str, amount: float) -> None:
         """
         This function will convert the amount
@@ -27,5 +41,10 @@ class CurrencyExchange:
                 get_available_currency_rates method 
             amount (float): Amount to be converted
         """
-        
 
+        amount_converted = self.currency_exchange.convert(base_cur=self.default_currency, dest_cur=currency, amount=amount)
+        print(amount_converted)
+
+currency_exchange = CurrencyExchange()
+
+currency_exchange.get_available_currency_rates()
