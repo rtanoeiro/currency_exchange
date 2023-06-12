e73fbf06530ea2f0d7e48db36ad530417f066869
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-06-12 12:56:57 +1000

    Updated and linted code

--- utils/currency_exchange.py
+++ utils/currency_exchange.py
@@ -1,36 +1,46 @@
 """Module to hold all functions to convert into different Currency Exchanges"""
 
-import datetime
-from io import BytesIO
-from urllib.request import urlopen, urlretrieve
-from zipfile import ZipFile
+from urllib.request import urlretrieve
 
 import pandas as pd
-import wget
 from currency_converter import ECB_URL, CurrencyConverter
 
 
 class CurrencyExchange:
     """
-    Class holding useful functions for Currency Exchange calculations    
+    Class holding useful functions for Currency Conversion calculations
+
+    The base currency for this Database is Euro. To make calculations on any of the
+    available currencies, the data file will be updated based on the base currency selected
     """
 
-    def __init__(self) -> None:
-        self.default_currency = "AUD"
+    def __init__(self, base_currency: str = "EUR") -> None:
+        """
+        Construction of class function
+
+        Args:
+            base_currency (str, optional): Currency to be used as base for all calculations.
+            Defaults to "EUR".
+        """
+        self.default_currency = base_currency
         self.currency_exchange = CurrencyConverter(currency_file=ECB_URL)
 
     def get_available_currency_rates(self) -> list:
         """
         Function to get all available currencies
+
+        Returns:
+            list: List of all avalulable currencies
         """
 
-        # TODO: STUDY urlopen, urlretrieve
-        file_path = urlretrieve(url=ECB_URL)
-        data = pd.read_csv(file_path[0], compression='zip')
-        available_currencies = [currency if len(currency)==3 else "" for currency in data.columns][1:-1]
+        file_path, _ = urlretrieve(url=ECB_URL)
+        data = pd.read_csv(file_path, compression="zip")
+        available_currencies = [
+            currency if len(currency) == 3 else "" for currency in data.columns
+        ][1:-1]
 
         return available_currencies
-    
+
     def convert_into(self, currency: str, amount: float) -> None:
         """
         This function will convert the amount
@@ -38,12 +48,15 @@ class CurrencyExchange:
 
         Args:
             currency (str): Currency. For a full list of currencies, use the
-                get_available_currency_rates method 
+                get_available_currency_rates method
             amount (float): Amount to be converted
         """
 
-        amount_converted = self.currency_exchange.convert(base_cur=self.default_currency, dest_cur=currency, amount=amount)
+        amount_converted = self.currency_exchange.convert(
+            base_cur=self.default_currency, dest_cur=currency, amount=amount
+        )
         print(amount_converted)
+
 
 currency_exchange = CurrencyExchange()
 
