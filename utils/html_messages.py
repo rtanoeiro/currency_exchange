43f1823ad18cca58d1ba8ea21dd2d87302201cca
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-06-12 23:10:04 +1000

    Created file to contain several default messages on email

---
+++ utils/html_messages.py
@@ -0,0 +1,43 @@
+REPORT_MESSAGE = f"""
+            <html>
+
+            <body>
+                <p>Hi,</p>
+                <p>Please find the attached Report.</p>
+                <br />
+                <p>Contact the data team directly if you have any questions.</p>
+                <p>Thank you!</p>
+                <br />
+                <p>Cheers,<br />
+                    The Data Team</p>
+                <br />
+                <p style="color:red;">
+                    Please do not reply to this email as it is auto-generated.
+                </p>
+            </body>
+
+            </html>
+        """
+
+DEFAULT_MESSAGE = f"""
+            <html>
+
+            <body>
+                <p>Hi,</p>
+                <p>Please find the photos of the latest currency exchanges.</p>
+                <br />
+                <p>Thank you!</p>
+                <br />
+                <p>Cheers,<br />
+                    Ramon</p>
+                <br />
+                <p style="color:red;">
+                    Please do not reply to this email as it is auto-generated.
+                </p>
+            </body>
+
+            </html>
+        """
+
+
+HTML_MESSAGES = {"REPORT_MESSAGE": REPORT_MESSAGE, "DEFAULT_MESSAGE": DEFAULT_MESSAGE}
